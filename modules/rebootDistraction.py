569dd85940f28ead38200178ec01c46b1c711af9
Michiel Hoerée <michielhoeree@hotmail.com>
2025-01-02 14:42:01 +0100

    reboot module gemaakt

--- modules/rebootDistraction.py
+++ modules/rebootDistraction.py
@@ -1,9 +1,17 @@
-import os
-import subprocess 
-
-
+import psutil
+from time import sleep
+import random
 # os.system("shutdown /r /t 0")
 # print("rebooting now... (maar niet echt want dat duurt te lang voor testing)")
 
 
-subprocess.run("wmic product get name", shell=True)
+running_apps = [proc.name() for proc in psutil.process_iter(['name'])]  # Get process names
+susapp = random.choice(list(dict.fromkeys(running_apps)))
+while True:
+    running_apps = [proc.name() for proc in psutil.process_iter(['name'])]
+    if susapp in running_apps:
+        print("rebooting now... (maar niet echt want dat duurt te lang voor testing)")
+        # os.system("shutdown /r /t 0")
+    else:
+        print("not rebooting")
+    sleep(3600)
